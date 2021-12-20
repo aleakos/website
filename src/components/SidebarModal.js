3bbc728663de7ed41f8b0a19011e26125916b32d
Alex Leakos <aleakos@gmail.com>
2021-12-20 00:49:38 -0700

    mobile changes sidebar

--- src/components/SidebarModal.js
+++ src/components/SidebarModal.js
@@ -10,38 +10,117 @@ let navStyle =
 
 const SidebarModal = () => {
   const [showModal, setShowModal] = useState(false);
-  const ref = useRef(null);
 
-  // // Allow to use the `esc` key
-  // useEffect(() => {
-  //   function handleEscape(event) {
-  //     if (!showModal) return;
+  const Modal = () => {
+    // setting ref inside modal because the component with ref was not being rendered when ref was called
+    const ref = useRef(null);
+    useEffect(() => {
+      const onBodyClick = (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+        setShowModal(false);
+      };
+      document.body.addEventListener('click', onBodyClick, { capture: true });
 
-  //     if (event.key === 'Escape') {
-  //       setShowModal(false);
-  //     }
-  //   }
+      // cleanup function to remove the event listener
+      return () => {
+        document.body.removeEventListener('click', onBodyClick, {
+          capture: true,
+        });
+      };
+    }, []);
 
-  //   document.addEventListener('keyup', handleEscape);
-  //   return () => document.removeEventListener('keyup', handleEscape);
-  // }, [showModal]);
-
-  useEffect(() => {
-    const onBodyClick = (event) => {
-      if (ref.current.contains(event.target)) {
-        return;
-      }
-      setShowModal(false);
-    };
-    document.body.addEventListener('click', onBodyClick, { capture: true });
-
-    // cleanup function to remove the event listener
-    return () => {
-      document.body.removeEventListener('click', onBodyClick, {
-        capture: true,
-      });
-    };
-  }, []);
+    return (
+      <>
+        <div
+          div
+          className="fixed inset-0 w-3/4 h-screen z-50 outline-none focus:outline-none"
+        >
+          <div className="relative">
+            {/*content*/}
+            <div
+              ref={ref}
+              onClick={() => console.log('clicked')}
+              className="border-0 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+            >
+              {/*header*/}
+              <div className="flex items-start justify-between p-5">
+                <h3 className="plex text-4xl font-semibold">menu.</h3>
+                <button
+                  className="plex inline-block p-1 ml-auto bg-transparent border-0 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={() => setShowModal(false)}
+                >
+                  <span className=" text-gray-300 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    ×
+                  </span>
+                </button>
+              </div>
+              {/*body*/}
+              <nav className="flex flex-col">
+                <NavLink
+                  exact
+                  to="/"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-home'}
+                >
+                  home
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/post"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-post'}
+                >
+                  Blog Posts
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/project"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-project'}
+                >
+                  Projects
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/about"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-about'}
+                >
+                  about
+                </NavLink>
+                <a
+                  href="https://github.com/aleakos"
+                  target="_blank"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-github'}
+                >
+                  github
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/alexander-leakos-a49081a3/"
+                  target="_blank"
+                  className={navStyle}
+                  onClick={() => setShowModal(false)}
+                  key={'sidebar-linkedin'}
+                >
+                  LinkedIn
+                </a>
+              </nav>
+              {/*footer*/}
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
+    );
+  };
 
   return (
     <>
@@ -55,95 +134,7 @@ const SidebarModal = () => {
           <span className="w-6 h-1 bg-gray-800 dark:bg-white mb-1"></span>
         </button>
       </div>
-      {showModal ? (
-        <>
-          <div
-            div
-            className="fixed inset-0 w-3/4 h-screen z-50 outline-none focus:outline-none"
-          >
-            <div className="relative">
-              {/*content*/}
-              <div
-                ref={ref}
-                onClick={() => console.log('clicked')}
-                className="border-0 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
-              >
-                {/*header*/}
-                <div className="flex items-start justify-between p-5">
-                  <h3 className="plex text-4xl font-semibold">menu.</h3>
-                  <button
-                    className="plex inline-block p-1 ml-auto bg-transparent border-0 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className=" text-gray-300 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <nav className="flex flex-col">
-                  <NavLink
-                    exact
-                    to="/"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-home'}
-                  >
-                    home
-                  </NavLink>
-                  <NavLink
-                    exact
-                    to="/post"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-post'}
-                  >
-                    Blog Posts
-                  </NavLink>
-                  <NavLink
-                    exact
-                    to="/project"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-project'}
-                  >
-                    Projects
-                  </NavLink>
-                  <NavLink
-                    exact
-                    to="/about"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-about'}
-                  >
-                    about
-                  </NavLink>
-                  <a
-                    href="https://github.com/aleakos"
-                    target="_blank"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-github'}
-                  >
-                    github
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/alexander-leakos-a49081a3/"
-                    target="_blank"
-                    className={navStyle}
-                    onClick={() => setShowModal(false)}
-                    key={'sidebar-linkedin'}
-                  >
-                    LinkedIn
-                  </a>
-                </nav>
-                {/*footer*/}
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+      {showModal ? <Modal /> : null}
     </>
   );
 };
